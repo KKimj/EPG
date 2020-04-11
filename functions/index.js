5db6a617f3be0bac854f1376e2d07a7a57697846
KKimj <kkimj@hanyang.ac.kr>
2020-04-11 17:27:41 +0900

    This is okay @ios, @android. but doesn't work @web. this is a big issue.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+var bodyParser = require('body-parser')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+
+exports.helloWorld_2 = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.blk = functions.https.onRequest((request, response) => {
+    response.send( request.body.text + "Hello");
+});
+   
+   
